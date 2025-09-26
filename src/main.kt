6fdb9474cbf2769f15f47d5ec3bc37cdec2fc8a2
2brooj75 <2brooj75@solent.ac.uk>
2025-09-26 09:55:51 +0100

    feat: created flathead and cupra cats

--- src/main.kt
+++ src/main.kt
@@ -5,4 +5,19 @@ fun main() {
     clyde.walk()
     println(binnie)
     println(clyde)
+
+
+    val flathead = Cat("Flathead", 7, 10)
+    val cupra = Cat("Cupra", 2, 7)
+
+    flathead.eat()
+    flathead.eat()
+    println(flathead)
+
+    cupra.walk()
+    cupra.walk()
+    cupra.walk()
+    cupra.walk()
+
+    println(cupra)
 }
